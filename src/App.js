ad4f94fd1de8308f1841f29e636147769436e930
brenobbfreitas <brenobbfreitas@gmail.com>
2021-11-08 20:45:48 -0300

    new cards

--- src/App.js
+++ src/App.js
@@ -3,14 +3,30 @@ import  FormularioCadastro  from "./components/FormularioCadastro";
 import ListaDeNotas from './components/ListaDeNotas';
 import "./assets/app.css";
 import "./assets/index.css"
-import CardNota from "./components/CardNota";
 class App extends Component {
+
+    constructor(){
+      super();
+
+      this.state = {
+        notas:[]
+      }
+    }
+
+    criarNota(titulo, texto){
+      const novaNota = {titulo, texto};
+      const novoArrayNotas = [...this.state.notas,novaNota]
+      const novoEstado ={
+        notas:novoArrayNotas
+      }
+      this.setState(novoEstado)
+    }
 
     render() {
       return (
-        <section className="container">
-          <FormularioCadastro/>
-          <ListaDeNotas/>
+        <section className="conteudo">
+          <FormularioCadastro criarNota={this.criarNota.bind(this)}/>
+          <ListaDeNotas notas={this.state.notas}/>
   
         </section>
     );
